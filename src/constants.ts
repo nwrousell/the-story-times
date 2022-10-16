8c19535d23e3f128bb7ddc3900a62f2f3647f0ea
Noah Rousell <59492853+nwrousell@users.noreply.github.com>
2022-10-16 03:49:55 -0400

    new images

--- src/constants.ts
+++ src/constants.ts
@@ -5,4 +5,4 @@ export const TIME_TO_SEE_VOTING_RESULTS = 5 // in seconds
 export const PROMPTS_PER_USER = 3
 export const IMAGES_PER_USER = 3
 export const CHANCE_OF_LIE = 0.3
-export const MAX_IMAGE_PATH = 15
+export const MAX_IMAGE_PATH = 25
